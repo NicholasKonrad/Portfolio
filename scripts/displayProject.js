cd4f5a732ec6e9a0c2ed754406cb78b74bdec419
electronicRevival <nicholas.konrad@mail.de>
2020-06-12 16:44:05 +0200

    Updated Portfolio

---
+++ scripts/displayProject.js
@@ -0,0 +1,25 @@
+const sections = document.getElementsByClassName('projectsection');
+const buttons = document.getElementById('card_projects').getElementsByClassName('navelem');
+
+function displayProject(v) { 
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+        buttons[i].style.borderWidth = '0px';
+    }    
+
+    if (sections.length-1 < v) {
+        sections[0].style.display = 'flex';
+        buttons[0].style.borderWidth = '1px';
+        currentTab = 0;
+    }
+    else if (v < 0) {
+        sections[sections.length-1].style.display = 'flex';
+        buttons[buttons.length-1].style.borderWidth = '1px';
+        currentTab = sections.length - 1;
+    }
+    else {
+        sections[v].style.display = 'flex';
+        buttons[v].style.borderWidth = '1px';
+        currentTab = v;
+    } 
+}
--- scripts/keyboardControl.js
+++ scripts/keyboardControl.js
@@ -3,13 +3,15 @@ const buttons = document.getElementById('card_projects').getElementsByClassName(
 
 let currentTab = 0;
 
+function isValidKey(eventcode) {
+    let array = ['F11', 'F5', 'F12', 'ArrowUp', 'ArrowDown', 'ControlLeft', 'ShiftLeft'];
+    for (let i = 0; array[i] < array.length; i++) {
+        if (eventcode == array[i]) return true;
+    }   
+}
+
 window.addEventListener('keydown', event => {   
-    if (event.code != 'F11' && 
-        event.code != 'F5' && 
-        event.code != 'F12' &&
-        event.code != 'ArrowUp' &&
-        event.code != 'ArroDown') 
-            event.preventDefault();    
+    if (isValidKey(event.code)) event.preventDefault();    
     
     if (event.code == 'ArrowLeft' && 0 <= currentPosition-1) scrollToSection(--currentPosition);
     if (event.code == 'ArrowRight' && currentPosition+1 < anchors.length) scrollToSection(++currentPosition);
@@ -20,26 +22,3 @@ window.addEventListener('keydown', event => {
     }    
 });
 
-function displayProject(v) { 
-    scrollToSection(2);
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-        buttons[i].style.borderWidth = '0px';
-    }    
-
-    if (sections.length-1 < v) {
-        sections[0].style.display = 'flex';
-        buttons[0].style.borderWidth = '1px';
-        currentTab = 0;
-    }
-    else if (v < 0) {
-        sections[sections.length-1].style.display = 'flex';
-        buttons[buttons.length-1].style.borderWidth = '1px';
-        currentTab = sections.length - 1;
-    }
-    else {
-        sections[v].style.display = 'flex';
-        buttons[v].style.borderWidth = '1px';
-        currentTab = v;
-    } 
-}
